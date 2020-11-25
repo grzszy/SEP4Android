e73cd53a3e0dd1e1ae4a3f5369da70b26de0a9f8
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2020-11-25 13:47:57 +0100

    Home_Repository Class finished
    Home_Repository class implemented.
    Forcast, Log, Warning Classes added to the model

--- app/src/main/java/com/example/mainactivity/Repositories/Home_Repository.java
+++ app/src/main/java/com/example/mainactivity/Repositories/Home_Repository.java
@@ -1,4 +1,57 @@
 package com.example.mainactivity.Repositories;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.mainactivity.Model.Current;
+import com.example.mainactivity.Model.Forcast;
+import com.example.mainactivity.Model.Log;
+import com.example.mainactivity.Model.Warning;
+import com.example.mainactivity.View.Forecast_Fragment;
+
 public class Home_Repository {
+    private static Home_Repository instance;
+
+    private MutableLiveData<Current> current;
+
+    private MutableLiveData<Forcast> forcast;
+
+    private MutableLiveData<Log> log;
+
+    private MutableLiveData<Warning> warning;
+
+    public static Home_Repository getInstance() {
+        return instance;
+    }
+
+    public LiveData<Current> getCurrent() {
+        return current;
+    }
+
+    public LiveData<Forcast> getForcast() {
+        return forcast;
+    }
+
+    public LiveData<Log> getLog() {
+        return log;
+    }
+
+    public LiveData<Warning> getWarning() {
+        return warning;
+    }
+
+    public void updateCurrent(Current nCurrent){
+        current.setValue(nCurrent);
+    }
+    public void updateForcast(Forcast nForcast){
+        forcast.setValue(nForcast);
+    }
+    public void updateLog(Log nLog){
+        log.setValue(nLog);
+    }
+    public void updateWarning(Warning nWarning){
+        warning.setValue(nWarning);
+    }
+
+
 }
