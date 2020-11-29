7e705727a23ff8810e6a6f62206b8f3b8d1f83fd
Jakob Busch <Jakob-busch@hotmail.com>
2020-11-30 00:32:14 +0100

    Progressbars value indicator, color + not null if statement

--- app/src/main/java/com/example/mainactivity/View/Home_Fragment.java
+++ app/src/main/java/com/example/mainactivity/View/Home_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.mainactivity.View;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -38,6 +39,7 @@ public class Home_Fragment extends Fragment {
     TextView textViewCO2;
     TextView textViewPeople;
 
+    Current currentTemp;
 
     @Nullable
     @Override
@@ -58,10 +60,41 @@ public class Home_Fragment extends Fragment {
         textViewCO2 = v.findViewById(R.id.textViewCO2);
         textViewPeople = v.findViewById(R.id.textViewPeople);
 
+        progressBarTemp.getProgressDrawable().setColorFilter(Color.RED,android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBarCO2.getProgressDrawable().setColorFilter(Color.GREEN,android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBarHumidity.getProgressDrawable().setColorFilter(Color.BLUE,android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBarPeople.getProgressDrawable().setColorFilter(Color.YELLOW,android.graphics.PorterDuff.Mode.SRC_IN);
+
+
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                home_viewModel.updateCurrent();
+                if (home_viewModel.getCurrent() != null)
+                {
+                    home_viewModel.updateCurrent();
+                }
+
+                progressBarTemp.setProgress(20);
+                progressBarCO2.setProgress(1200);
+                progressBarHumidity.setProgress(40);
+                progressBarPeople.setProgress(30);
+
+                if (progressBarTemp != null ){
+
+                    textViewTemp.setText(progressBarTemp.getProgress()+"°C");
+                }
+                if (progressBarCO2 != null ){
+
+                    textViewCO2.setText(progressBarCO2.getProgress()+" ppm");
+                }
+                if (progressBarHumidity != null ){
+
+                    textViewHumidity.setText(progressBarHumidity.getProgress()+"%");
+                }
+                if (progressBarPeople != null ){
+
+                    textViewPeople.setText(progressBarPeople.getProgress()+"/h");
+                }
             }
         });
 
@@ -70,10 +103,13 @@ public class Home_Fragment extends Fragment {
         progressBarHumidity.setMax(100);
         progressBarPeople.setMax(200);
 
+
+
         home_viewModel = new ViewModelProvider(this).get(Home_ViewModel.class);
         home_viewModel.getCurrent().observe(getActivity(), new Observer<Current>() {
             @Override
             public void onChanged(Current current) {
+
 
 
                 Home_Fragment.this.progressBarTemp.setProgress((int) current.getTemp_value());
@@ -81,14 +117,12 @@ public class Home_Fragment extends Fragment {
                 Home_Fragment.this.progressBarCO2.setProgress(current.getCO2_value());
                 Home_Fragment.this.progressBarPeople.setProgress(current.getPassenger_value());
 
-                Home_Fragment.this.textViewTemp.setText(current.getTemp_value() + "");
-                Home_Fragment.this.textViewHumidity.setText(current.getHumidity_value() + "%");
-                Home_Fragment.this.textViewCO2.setText(current.getCO2_value() + "");
-                Home_Fragment.this.textViewPeople.setText(current.getPassenger_value() + "");
+
+
+
 
             }
         });
-
         
         return v;
     }
