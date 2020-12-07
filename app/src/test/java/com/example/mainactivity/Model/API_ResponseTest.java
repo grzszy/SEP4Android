4af8ad0e57a0bf98faccca23c7670b98c76ee38a
Marwan Summakieh <42711091+MarwanSummakieh@users.noreply.github.com>
2020-12-07 10:02:48 +0100

    testing

---
+++ app/src/test/java/com/example/mainactivity/Model/API_ResponseTest.java
@@ -0,0 +1,17 @@
+package com.example.mainactivity.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class API_ResponseTest {
+
+    @org.junit.jupiter.api.Test
+    void getCurrent() {
+        Current current = new Current(1,2,3,4);
+        assertEquals(1,1);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void postShaft() {
+    }
+}
