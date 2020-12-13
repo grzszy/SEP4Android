ec5e56d294d9d516b1452898b4bf183cba914e36
Irongarden <mathiashave@me.com>
2020-12-13 16:30:50 +0100

    Log IMPL.

--- app/src/main/java/com/example/mainactivity/Model/Log.java
+++ app/src/main/java/com/example/mainactivity/Model/Log.java
@@ -1,5 +1,20 @@
 package com.example.mainactivity.Model;
 
+import androidx.lifecycle.LiveData;
+
+import java.util.ArrayList;
+
 public class Log {
+
+    private ArrayList<LiveData<Forecast>> logList;
+
+    public Log(ArrayList<LiveData<Forecast>> logList) {
+        this.logList = logList;
+    }
+
+    public ArrayList<LiveData<Forecast>> getLogList() {
+        return logList;
+    }
+
 
 }
