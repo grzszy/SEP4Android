5e0d01b1d2e186438f5a50781d8bd6529e711707
Grzegorz <g.szyszka95@gmail.com>
2020-12-14 15:07:36 +0100

    Revert "Revert "Merge branch 'master' into Greg""
    This reverts commit fa33d657c662fbe1626a0a2c7fa7d7068430d678.

--- app/src/main/java/com/example/mainactivity/Model/Log.java
+++ app/src/main/java/com/example/mainactivity/Model/Log.java
@@ -1,28 +1,20 @@
 package com.example.mainactivity.Model;
 
-public class Log {
-<<<<<<< Updated upstream
-=======
+import androidx.lifecycle.LiveData;
 
+import java.util.ArrayList;
+
+public class Log {
 
     private ArrayList<LiveData<Forecast>> logList;
 
-    /**
-     * Setter for log list.
-     * @param logList
-     */
     public Log(ArrayList<LiveData<Forecast>> logList) {
         this.logList = logList;
     }
 
-    /**
-     * Getter gor log list.
-     * @return logList
-     */
     public ArrayList<LiveData<Forecast>> getLogList() {
         return logList;
     }
 
 
->>>>>>> Stashed changes
 }
